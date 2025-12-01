3ea61b6ac6f40bb6247dde553f01c9227c6407a3
Alan Lockett <alockett@Alans-MacBook-Pro.local>
2025-12-01 11:45:42 -0600

    switch calling pattern based on flax version

--- src/jax_notes/convnet.py
+++ src/jax_notes/convnet.py
@@ -537,4 +537,5 @@ if __name__ == "__main__":
             train_acc = metrics_history['train_accuracy'][-1]
             test_loss = metrics_history['test_loss'][-1]
             test_acc = metrics_history['test_accuracy'][-1] 
-            print(f"Step {step} LR: {optimizer.learning_rate:.4e}, train loss: {train_loss:.4f}, acc: {train_acc:.4f}, test loss: {test_loss:.4f}, acc: {test_acc:.4f}")
+            lr = schedule(optimizer.state.count)
+            print(f"Step {step} LR: {lr:.4e}, train loss: {train_loss:.4f}, acc: {train_acc:.4f}, test loss: {test_loss:.4f}, acc: {test_acc:.4f}")
